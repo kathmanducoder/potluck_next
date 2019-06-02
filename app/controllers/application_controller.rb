ad4cdbd8ae68e66ede3e241972bc85509639cb04
kathmanducoder <kathmanducoder@gmail.com>
2019-06-02 10:00:41 -0400

    Add two helper methods: logged_in? and current_user
    logged_in? returns true if a user is logged in to website.
    current_user returns the user that is currently logged in. If not logged in, return nil.
    
    Update index action to route to potlucks_path if logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
 class ApplicationController < ActionController::Base
 
+  helper_method :current_user
+
   def index
+    if logged_in?
+      redirect_to potlucks_path
+    end
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user
+    if logged_in?
+      User.find_by(id:session[:user_id])
+    end
   end
 
 end
