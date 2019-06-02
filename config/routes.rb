61596d0333a362f73c366253266aeb94f83d651d
kathmanducoder <kathmanducoder@gmail.com>
2019-06-01 23:13:40 -0400

    Add routes for post to login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "application#index"
   resources :users
   resources :potlucks
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 end
