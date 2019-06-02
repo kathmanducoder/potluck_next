61892f21926629609b943e114bfbf02b29c86111
kathmanducoder <kathmanducoder@gmail.com>
2019-06-01 23:13:10 -0400

    Ran rake db:migrate after creating migration for potlucks.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_014456) do
+ActiveRecord::Schema.define(version: 2019_06_02_023637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "potlucks", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.integer "organizer_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
