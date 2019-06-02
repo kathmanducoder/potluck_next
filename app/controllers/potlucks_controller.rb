d67127106e49700dc02b201b5b867b4a77c2771b
kathmanducoder <kathmanducoder@gmail.com>
2019-06-02 10:19:06 -0400

    Create a partial form for potlucks. Render that partial from new view. Update controller action create to handle post for potluck creation. Display all the potlucks in index file. Add a link to create a new potluck in potlucks index file. Create an scss file to manage all css classes in potlucks.

--- app/controllers/potlucks_controller.rb
+++ app/controllers/potlucks_controller.rb
@@ -1,9 +1,36 @@
 class PotlucksController < ApplicationController
+  before_action :set_potluck, only: [:show]
 
   def index
+    @potlucks = Potluck.all
+  end
+
+  def new
+    @potluck = Potluck.new
+  end
+
+  def create
+    potluck = Potluck.new(potluck_params)
+    potluck.organizer = current_user
+    potluck.save
+    redirect_to potlucks_path
+  end
+
+  def show
   end
 
   def data
+  end
+
+
+  private
+
+  def set_potluck
+    @potluck = Potluck.find_by(id: params[:id])
+  end
+
+  def potluck_params
+    params.require(:potluck).permit(:name, :date)
   end
 
 end
