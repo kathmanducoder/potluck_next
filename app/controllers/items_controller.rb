4905c8775bd2ecf5407b1fb4a61b2052afd728f7
kathmanducoder <kathmanducoder@gmail.com>
2019-06-02 21:40:24 -0400

    Update potluck's show page to display a link to add a new item. Also display all the items belonging to the potluck in potluck's show page. Add code to create an item.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,31 @@
 class ItemsController < ApplicationController
 
+  before_action :set_item, only: [:show]
+
+  def new
+    @potluck = Potluck.find_by(id: params[:potluck_id])
+    @item = @potluck.items.build
+  end
+
+  def create
+    potluck = Potluck.find_by(id: params[:potluck_id])
+    item = potluck.items.build(item_params)
+    item.user = current_user
+    item.save
+    redirect_to potluck
+  end
+
+  def show
+  end
+
+  private
+
+  def set_item
+    @item = Item.find_by(id: params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :potluck_id)
+  end
+
 end
